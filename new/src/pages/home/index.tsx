9e7eca6a97a2a9a5561484c0f4fad0e0435e48ab
Asadbek <asadbeknan5566@gmail.com>
2025-11-09 23:32:18 +0500

    Oh No

--- new/src/pages/home/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function HomePage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <Button>Click</Button>
-    </div>
-  );
-}
